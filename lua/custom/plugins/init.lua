ebdfa733781726439c284772e0b8a3aad5e60b12
toon159 <toonbcc@gmail.com>
2024-10-18 13:36:46 +0700

    add nerd font use web-devicons

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,8 +8,8 @@ return {
     lazy = false,
     opts = {},
     -- Optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
+    -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
     keys = {
       { '-', '<cmd>Oil<cr>', desc = 'Open parent directory' },
     },
