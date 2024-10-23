591b12ef50e523a972e4e5dc958e81a8d9de0007
toon159 <toonbcc@gmail.com>
2024-10-23 21:15:24 +0700

    add mini files

--- init.lua
+++ init.lua
@@ -884,6 +884,7 @@ require('lazy').setup({
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
+      require('mini.files').setup()
     end,
   },
   { -- Highlight, edit, and navigate code
